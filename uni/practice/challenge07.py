be275ab1b7fa0ed140a3dbb7e4a991821ef97570
aatharvauti <bm.aatharvauti@protonmail.ch>
2022-05-24 20:38:40 +0100

    Rename uni

--- uni/practice/challenge07.py
+++
@@ -1,5 +0,0 @@
-import pyqrcode
-from pyqrcode import QRCode
-s = "https://aatharvauti.github.io/portfolio"
-url = pyqrcode.create(s)
-url.png('myqr.png', scale = 6)
--- uni/practice/challenge10.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to read each row from a given csv file and print a list of strings.
-
-import csv
-
-with open('sample.csv', newline='') as csvfile:
-    data = csv.reader(csvfile, delimiter=';', quotechar='|')
-    for row in data:
-        print(', '.join(row))
